032b104d38534211634caee4674680a58e143fa3
M Rafi Juliansyah <mrafijuliansyah@gmail.com>
2015-06-25 10:40:38 +0700

    commit test

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
+use Carbon\Carbon;
+use Illuminate\httpResponse;
+use App\Http\Controllers\Controller;
+
+class ArticlesController extends Controller
+{
+    //
+    public function index() {
+
+    
+    $articles = Article::latest('published_at')->published()->get();
+ 
+    return view('article.index',compact('articles'));
+}
+	public function show($id) {
+		$article=Article::findOrFail($id); 
+
+		return view('article.show',compact('article'));
+
+	}
+
+	public function create(){
+		return view('article.create');
+	}    
+
+	public function store(ArticleRequest $request){
+ 		Article::create($request->all());
+
+		return redirect('articles');
+	} 
+
+	public function edit($id){
+		$article = Article::findOrFail($id);
+		return view('article.edit',compact('article'));
+	}
+	public function update($id,ArticleRequest $request){
+		$article = Article::findOrFail($id);
+
+		$article->update($request->all());
+
+		return redirect ('articles');
+
+	}
+}
